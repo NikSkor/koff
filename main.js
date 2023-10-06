869a1f9966b1b0b125431f394cb8374f8f1c1fb5
NikSkor <mr-zerx@yandex.ru>
2023-10-06 06:38:18 +0500

    main in progress

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 import 'normalize.css';
 import './style.scss';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     Hello JS
-//   </div>
-// `;
+const swiperThumbnails = new Swiper('.product__slider-thumbnails', {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+const swiper = new Swiper('.product__slider-main', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.product__arrow_next',
+    prevEl: '.product__arrow_prev',
+  },
+  thumbs: {
+    swiper: swiperThumbnails,
+  },
+  modules: [Navigation, Pagination],
+});
