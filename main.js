b1d988319c9284c1e3409c144bfa1e818908a672
NikSkor <mr-zerx@yandex.ru>
2023-10-09 05:22:04 +0500

    added navigo

--- main.js
+++ main.js
@@ -1,25 +1,63 @@
 import 'normalize.css';
 import './style.scss';
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import Navigo from 'navigo';
+// import Swiper from 'swiper';
+// import { Navigation, Pagination } from 'swiper/modules';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
 
-const swiperThumbnails = new Swiper('.product__slider-thumbnails', {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-const swiper = new Swiper('.product__slider-main', {
-  spaceBetween: 10,
-  navigation: {
-    nextEl: '.product__arrow_next',
-    prevEl: '.product__arrow_prev',
-  },
-  thumbs: {
-    swiper: swiperThumbnails,
-  },
-  modules: [Navigation, Pagination],
-});
+
+
+const productSlider = () => {
+  Promise.all([
+    import('swiper/modules'),
+    import('swiper'),
+    import('swiper/css'),
+  ]).then(([{ Navigation, Thumbs }, Swiper]) => {
+    const swiperThumbnails = new Swiper.default('.product__slider-thumbnails', {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+    new Swiper.default('.product__slider-main', {
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '.product__arrow_next',
+        prevEl: '.product__arrow_prev',
+      },
+      modules: [Navigation, Thumbs],
+      thumbs: {
+        swiper: swiperThumbnails,
+      },
+    });
+  });
+};
+
+
+
+const init = () => {
+  productSlider();
+
+  const router = new Navigo('/', {linksSelector: 'a[href^="/"]'});
+
+  router
+    .on('/', () => {})
+    .on('/category', () => {
+      console.log('category');
+    })
+    .on('/favourite', () => {})
+    .on('/search', () => {})
+    .on('/product/:id', (obj) => {})
+    .on('/cart', () => {})
+    .on('/order', () => {})
+    .notFound(() => {
+      document.body.innerHTML = '<h2>Страница не найдена</h2>'
+    });
+
+  router.resolve();
+
+}
+
+init();
